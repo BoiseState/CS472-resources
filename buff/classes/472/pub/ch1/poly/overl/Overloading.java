2add44bfc10bd5798c00e034753b4df831e955b6
Jim Buffenbarger <buff@cs.boisestate.edu>
2024-01-08 18:00:10 -0700

    update pubs: Mon Jan  8 06:00:10 PM MST 2024

--- buff/classes/472/pub/ch1/poly/overl/Overloading.java
+++ buff/classes/472/pub/ch1/poly/overl/Overloading.java
@@ -2,14 +2,14 @@
 
 public class Overloading {
 
-    private static void foo(int i)    { System.out.println("foo(int i)"); }
-    private static void foo(double d) { System.out.println("foo(double d)"); }
+  private static void foo(int i)    { System.out.println("foo(int i)"); }
+  private static void foo(double d) { System.out.println("foo(double d)"); }
 
-    public static void main(String[] args) {
-	int i=77;
-	double d=1.234;
-	foo(i);
-	foo(d);
-    }
-	
+  public static void main(String[] args) {
+    int i=77;
+    double d=1.234;
+    foo(i);
+    foo(d);
+  }
+
 }
