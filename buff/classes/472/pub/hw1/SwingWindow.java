4007abae5246692eef20546840f7481e3126574a
Jim Buffenbarger <buff@cs.boisestate.edu>
2024-02-05 12:00:04 -0700

    update pubs: Mon Feb  5 12:00:04 PM MST 2024

--- buff/classes/472/pub/hw1/SwingWindow.java
+++ buff/classes/472/pub/hw1/SwingWindow.java
@@ -7,64 +7,64 @@ import javax.swing.*;
 
 public class SwingWindow implements Window {
 
-    private final int width=200;
-    private final int height=200;
+  private final int width=200;
+  private final int height=200;
 
-    private Window _window;
-    private Glyph _glyph;
-    private Pane _pane;
-    private JFrame _jFrame;
-    private Graphics _graphics;
-    private FontMetrics _fm;
+  private Window _window;
+  private Glyph _glyph;
+  private Pane _pane;
+  private JFrame _jFrame;
+  private Graphics _graphics;
+  private FontMetrics _fm;
 
-    private class Pane extends JPanel {
+  private class Pane extends JPanel {
 
-	public void paintComponent(Graphics graphics) {
-	    super.paintComponent(graphics);
-	    _graphics=graphics;
-	    if (_glyph!=null)
-                _glyph.draw(_window);
-	}
-
+    public void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      _graphics=graphics;
+      if (_glyph!=null)
+        _glyph.draw(_window);
     }
 
-    public SwingWindow(String title) {
-	_window=this;
-	JFrame.setDefaultLookAndFeelDecorated(true);
-	_jFrame=new JFrame(title);
-        _jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        _jFrame.setJMenuBar(new JMenuBar());
-	_jFrame.pack(); // from: aaronrussell@u.boisestate.edu
-        _jFrame.setSize(width,height);
-        _jFrame.setVisible(true);
-	_fm=_jFrame.getGraphics().getFontMetrics();
-    }
+  }
 
-    public int charWidth(char c) {
-	return _fm.charWidth(c);
-    }
+  public SwingWindow(String title) {
+    _window=this;
+    JFrame.setDefaultLookAndFeelDecorated(true);
+    _jFrame=new JFrame(title);
+    _jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    _jFrame.setJMenuBar(new JMenuBar());
+    _jFrame.pack();
+    _jFrame.setSize(width,height);
+    _jFrame.setVisible(true);
+    _fm=_jFrame.getGraphics().getFontMetrics();
+  }
 
-    public int charHeight(char c) {
-	return _fm.getHeight();
-    }
+  public int charWidth(char c) {
+    return _fm.charWidth(c);
+  }
 
-    private int charBaseline(char c) {
-	return _fm.getLeading()+_fm.getAscent();
-    }
+  public int charHeight(char c) {
+    return _fm.getHeight();
+  }
 
-    public void drawCharacter(char c, int x, int y) {
-	_graphics.drawString(c+"",x,y+charBaseline(c));
-    }
+  private int charBaseline(char c) {
+    return _fm.getLeading()+_fm.getAscent();
+  }
 
-    public void drawRectangle(int x, int y, int width, int height) {
-	_graphics.drawRect(x,y,width,height);
-    }
+  public void drawCharacter(char c, int x, int y) {
+    _graphics.drawString(c+"",x,y+charBaseline(c));
+  }
 
-    public void setContents(Glyph glyph) {
-	_glyph=glyph;
-	_pane=new Pane();
-	_jFrame.setContentPane(_pane);
-        _jFrame.setVisible(true);
-    }
+  public void drawRectangle(int x, int y, int width, int height) {
+    _graphics.drawRect(x,y,width,height);
+  }
+
+  public void setContents(Glyph glyph) {
+    _glyph=glyph;
+    _pane=new Pane();
+    _jFrame.setContentPane(_pane);
+    _jFrame.setVisible(true);
+  }
 
 }
--- buff/classes/472/pub/hw1/Window.java
+++ buff/classes/472/pub/hw1/Window.java
@@ -4,12 +4,12 @@ import glyph.*;
 
 public interface Window {
 
-    void drawCharacter(char c, int x, int y);
-    void drawRectangle(int x, int y, int width, int height);
+  void drawCharacter(char c, int x, int y);
+  void drawRectangle(int x, int y, int width, int height);
 
-    int charWidth(char c);
-    int charHeight(char c);
+  int charWidth(char c);
+  int charHeight(char c);
 
-    void setContents(Glyph glyph);
+  void setContents(Glyph glyph);
 
 }
