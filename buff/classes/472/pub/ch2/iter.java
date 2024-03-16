316bc9523c2ddc81f80df7a6be06e7eb24f764a4
Jim Buffenbarger <buff@cs.boisestate.edu>
2024-03-16 12:00:04 -0600

    update pubs: Sat Mar 16 12:00:04 PM MDT 2024

--- buff/classes/472/pub/ch2/iter.java
+++ buff/classes/472/pub/ch2/iter.java
@@ -3,14 +3,13 @@ void next();
 boolean isDone();
 Glyph getCurrent();
 
-...
+// ...
 
 {
     Glyph g;
-    ...
+    // ...
     for (g.first(PREORDER); !g.isDone(); g.next()) {
-	Glyph current=g.getCurrent();
-	// do some analysis
+     	Glyph current=g.getCurrent();
+  	  // do some analysis
     }
 }
-
