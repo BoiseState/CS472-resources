7148c50c890ccd48ffc20d4b86f75772003e9540
Jim Buffenbarger <buff@cs.boisestate.edu>
2024-04-04 18:00:04 -0600

    update pubs: Thu Apr  4 06:00:04 PM MDT 2024

--- buff/classes/472/pub/hw5/KeyMap.java
+++ buff/classes/472/pub/hw5/KeyMap.java
@@ -4,14 +4,14 @@ import java.util.*;
 
 public class KeyMap {
 
-    private Map<String,Command> map=new HashMap<String,Command>();
+  private Map<String,Command> map=new HashMap<String,Command>();
 
-    public Command get(char c) {
-	return map.get(c+"");
-    }
+  public Command get(char c) {
+    return map.get(c+"");
+  }
 
-    public void put(char c, Command command) {
-	map.put(c+"",command);
-    }
+  public void put(char c, Command command) {
+    map.put(c+"",command);
+  }
 
 }
